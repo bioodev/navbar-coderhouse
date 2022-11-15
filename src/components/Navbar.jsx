7ba8669b22253e259e9e6e15825d10a13add6f94
bioodev <bioodev@protonmail.com>
2022-11-15 00:10:33 -0300

    subiendo navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import logo from "../images/logo192.png";
+import './navbar.css';
+
+const Navbar = () => {
+
+    const url = "http://localhost:3000/"
+  return (
+    <div className="contenedor-navbar">
+      <div className="logo">
+        <a href={url}>
+        <img src={logo} alt="" />
+        <h1>Navbar Coderhouse</h1>
+        </a>
+      </div>
+      <div className="listado-rutas">
+        <ul className="lista">
+          <li>
+            <a href={url}>Inicio</a>
+            </li>
+          <li>
+            <a href={url}>Productos</a>
+            </li>
+          <li>
+            <a href={url}>FAQ</a>
+            </li>
+          <li>
+            <a href={url}>Planes</a>
+            </li>
+          <li>
+            <a href={url}>Contacto</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
